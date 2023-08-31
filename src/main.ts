50f35cce7e41a19d6460987c9610b695289629a6
lebrancconvas <lebrancconvas@gmail.com>
2023-08-31 14:15:28 +0700

    🎉 feat(init): init project

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,42 @@ canvas.height = config.height;
 canvas.style.backgroundColor = config.backgroundColor;
 
 // Code.
+class Circle {
+  x: number;
+  y: number;
+  size: number;
+  color?: string;
+
+  constructor(x: number, y: number, size: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = this.color || 'White';
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const circle = new Circle(canvas.width / 2, canvas.height / 2, 10, 'Green');
+const circles: Circle[] = [];
+
+window.addEventListener('mousedown', (event: MouseEvent) => {
+  circle.x = event.clientX;
+  circle.y = event.clientY;
+  circles.push(circle);
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  circles.forEach((circle: Circle) => {
+    circle.draw();
+  });
+};
+
+animate();
